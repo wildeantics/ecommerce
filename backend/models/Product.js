93ffa8f3fe5906eaee49f05665b35ad17a66d833
Morgan Wilde <morgan@wilde.mx>
2022-06-09 16:30:44 +1000

    Changed schema to better load data

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -7,8 +7,9 @@ const ProductSchema = new mongoose.Schema(
     img: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: Array },
-    size: { type: String },
-    color: { type: String },
+    size: { type: Array },
+    color: { type: Array },
+    inStock: { type: Boolean, default: true },
   },
   { timestamps: true }
 )
