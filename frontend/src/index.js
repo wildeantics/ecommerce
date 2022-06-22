03700f1e80bf4aaffcfdbed8d069dd0caaf5ab89
Morgan Wilde <morgan@wilde.mx>
2022-06-22 16:21:54 +1000

    Attempt to fix API calls

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
-import { store, persistor } from './redux/store'
-import { PersistGate } from 'redux-persist/integration/react'
+import { store } from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 )
